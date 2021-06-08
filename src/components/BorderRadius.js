245e7f7ec9bf83ad3c6beb43eeb4ddf90297807f
Fiona Diep Nguyen <56050215+FionaDN87@users.noreply.github.com>
2021-06-07 22:04:20 -0500

    Display raw data

--- src/components/BorderRadius.js
+++ src/components/BorderRadius.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import Box from '@material-ui/core/Box';
-import { borderColor, color } from '@material-ui/system';
-import '../style/BorderRadius.css'
+import React from 'react'
+import'../style/BorderRadius.css'
+import { parse } from 'papaparse'
+import { RemoveCircleOutlineRounded } from '@material-ui/icons';
 
-const defaultProps = {
-  bgcolor: 'background.paper',
-  borderColor: 'text.primary',
-  m: 1,
-  border: 1,
-  style: { width: '15rem', height: '1.5rem' },
-  
-};
 
 export default function BorderRadius() {
     const [highlighted, setHighlighted] = React.useState(false);
-
+    const [datas, setData] = React.useState([
+      {
+      case_id: "XXXXXXX", 
+      faa_ors:"blank", 
+      faa_asn:"blank", 
+      usgs_pr_id:"XXXX", 
+      eia_id:"XXXXX",
+      t_state:"State",
+      t_country:"Country",
+      t_fips:"XXXX",
+      p_name:"Turbine Name",
+      p_year:"XXXX",
+      p_tnum:"XXX",
+      p_cap: "XX.XX",
+      t_manu:"Manu",
+      t_model:"blank",
+      t_cap:"95",
+    }
+    ]);
   return (
     <div>
         <h1 style={{textAlign:"center"}}>Import File</h1>
@@ -36,14 +46,34 @@ export default function BorderRadius() {
             //Convert file list to array and map
             Array.from(e.dataTransfer.files)
             .filter((file) => file.type ==="text/csv"    //filter only csv
-            ).forEach(file => {                          //iteratte each element in array
-                console.log(file)
-            });
-        }}
+            ).forEach(async file => {                    //iteratte each element in array  
+                
+                const text =await file.text();
+                const result = parse(text, {header:true})
+                //console.log(text);                    //Display raw file imported
+                setData(existing => [...existing, ...result.data])
+                console.log(result.data.length);      //Check array length
+                
+              
+                
+            }
+            )       
+        }
+      }
       >
               DROP HERE (.csv)
         </div>
         </div> </center>
+        <ul>
+          {datas.slice(0,50).map((data)=>(
+            <li key={data.case_id}>
+              {data.case_id}   |  {data.p_name}
+            </li>
+          ))}
+        </ul>
+        
+        
     </div>
+    
   );
 }
